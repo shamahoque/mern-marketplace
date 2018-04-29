547d8d3528549a510a7bd731fe01f8b6f9f75580
Shama Hoque <shama.hoque@gmail.com>
2018-04-29 17:19:26 +0600

    cleanup ;

--- server/controllers/shop.controller.js
+++ server/controllers/shop.controller.js
@@ -107,7 +107,7 @@ const update = (req, res, next) => {
 }
 
 const isOwner = (req, res, next) => {
-  const isOwner = req.shop && req.auth && req.shop.owner._id == req.auth._id;
+  const isOwner = req.shop && req.auth && req.shop.owner._id == req.auth._id
   if(!isOwner){
     return res.status('403').json({
       error: "User is not authorized"
